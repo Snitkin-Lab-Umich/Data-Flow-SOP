948ac2406b45f6faf9c2b2be3384bf9e1985933a
dhatribadri <dhatribadri@gwu.edu>
2024-10-08 15:19:54 -0400

    Updated the way samples are called

--- rename_samples.sh
+++ rename_samples.sh
@@ -35,25 +35,46 @@ while IFS=, read -r project sample_id description barcode reads; do
     description=$(echo "$description" | sed 's/"//g')
 
     #echo "Processing sample: $sample_id"  
+    #echo "Processing description: $description" 
 
     # Find matching FASTQ files in the directory
-    for fastq_file in "$fastq_directory"/*_R[12]_*.fastq.gz; do
+    for fastq_file in "$fastq_directory"/*.fastq.gz; do
         # Check if the file exists
         if [ -e "$fastq_file" ]; then
             base_name=$(basename "$fastq_file")
-            read_number=$(echo "$base_name" | grep -oP '_R[12]')
-            sample_base=$(echo "$base_name" | xargs | sed -E "s/_R[12]_.*//")
 
-            #echo "Base Name: $sample_base, Sample ID: $sample_id, Read Number: $read_number"  
+            # Check if the file ends with '_001' and remove it if present
+            if [[ "$base_name" == *_001.fastq.gz ]]; then
+                base_name=$(echo "$base_name" | sed 's/_001//')
+            fi
             
+             # Check if the base_name contains '_SXXX' preceding '_R1' or '_R2' and remove '_SXXX'
+            if [[ "$base_name" =~ _S[0-9]+_R[12] ]]; then
+                new_base_name=$(echo "$base_name" | sed -E 's/_S[0-9]+//')
+            else
+                new_base_name="$base_name"  # If no '_SXXX' found, keep the base name unchanged
+            fi
+
+            # Display base name before and after
+            #echo "Original Base Name: $base_name"
+            #echo "Updated Base Name: $new_base_name"
+
+            read_number=$(echo "$new_base_name" | grep -oP 'R[12]')
+            sample_base=$(echo "$new_base_name" | xargs | sed -E "s/_R[12].*//")
+
+            #echo "read num is: $read_number and sample base is $sample_base"
             # Check if sample_base matches the sample ID from the CSV
             if [[ $sample_base == $sample_id ]]; then  
-                new_file="${fastq_directory}/${description}${read_number}.fastq.gz"
-                echo "Renaming sample $fastq_file to $new_file"
-                echo "mv $fastq_file $new_file" >> "$log_file"
+                new_file="${fastq_directory}/${description}_${read_number}.fastq.gz"
                 
-                mv "$fastq_file" "$new_file"
-                rename_fastq=true
+                # echo "Renaming sample $fastq_file to $new_file"
+                # echo "mv $fastq_file $new_file" >> "$log_file"
+                # rename_fastq=true
+                if mv "$fastq_file" "$new_file":
+                    echo "Renaming sample $fastq_file to $new_file"
+                    echo "mv $fastq_file $new_file" >> "$log_file"
+                    rename_fastq=true
+                fi
             fi
         fi
     done
